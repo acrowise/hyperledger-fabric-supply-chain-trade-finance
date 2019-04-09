377b1416f4233f0c43eacff2597131ea3128d60b
alexdnn11 <aliaksandr.dubinin@altoros.com>
2019-04-09 15:04:53 +0300

    build client script fixed

--- scripts/build-client.sh
+++ scripts/build-client.sh
@@ -2,18 +2,8 @@
 
 ARCH=$(uname -s | grep Darwin)
 
-if [ "$ARCH" == "Darwin" ]; then
 
 docker run -it --rm \
     -v $(pwd)/client:/usr/src/app \
     node:8 \
     bash -c "cd /usr/src/app && npm install && npm run build"
-
-else
-
-docker run -it --rm \
-    -v $(pwd)/client:/usr/src/app \
-    -u $(id -u ${USER}) \
-    node:8 \
-    bash -c "cd /usr/src/app && npm install && npm run build"
-fi
