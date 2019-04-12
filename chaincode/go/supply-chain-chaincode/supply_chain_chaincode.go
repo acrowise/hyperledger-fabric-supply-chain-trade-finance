5de466f9e8bfc6484b8d10d8f697a889a95b39f7
Ilya Patotski <ilya.patotski@altoros.com>
2019-04-12 18:34:00 +0300

    Added chaincode skeletons.

---
+++ chaincode/go/supply-chain-chaincode/supply_chain_chaincode.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
+)
+
+var logger = shim.NewLogger("SupplyChainChaincode")
+
+type SupplyChainChaincode struct {
+}
+
+func (cc *SupplyChainChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	logger.Debug("Init")
+
+	return shim.Success(nil)
+}
+
+func (cc *SupplyChainChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	logger.Debug("Invoke")
+
+	function, args := stub.GetFunctionAndParameters()
+	if function == "placeOrder" {
+		// Buyer places order
+		return cc.placeOrder(stub, args)
+	} else if function == "acceptOrder" {
+		// Supplier accepts order, a new contract is stored in a Buyer-Supplier collection
+		return cc.acceptOrder(stub, args)
+	} else if function == "generateProof" {
+		// Supplier generates a proof for an Auditor
+		return cc.generateProof(stub, args)
+	} else if function == "acceptContract" {
+		// Buyer or Auditor accepts the contract, acceptance details are stored in the Buyer-Supplier collection
+		return cc.acceptContract(stub, args)
+	} else if function == "rejectContract" {
+		// Buyer or Auditor rejects the contract, acceptance details are stored in the Buyer-Supplier collection
+		return cc.rejectContract(stub, args)
+	} else if function == "listOrders" {
+		// List all orders
+		return cc.listOrders(stub, args)
+	} else if function == "listContracts" {
+		// List contracts for the party from every collection
+		return cc.listContracts(stub, args)
+	} else if function == "getProofForContract" {
+		// Returns a proof for the Auditor for the specified contract
+		return cc.getProofForContract(stub, args)
+	} else if function == "listAcceptances" {
+		// List all acceptance details for the contract
+		return cc.listAcceptances(stub, args)
+	}
+	// (optional) add other query functions
+
+	fnList := "{placeOrder, acceptOrder, generateProof, acceptContract, listOrders, listContracts, getProofForContract, listAcceptances}"
+	message := fmt.Sprintf("invalid invoke function name: expected one of %s, got %s", fnList, function)
+	logger.Debug(message)
+
+	return pb.Response{Status: 400, Message: message}
+}
+
+func (cc *SupplyChainChaincode) placeOrder(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// args: <order fields>
+	// check role == Buyer
+	// validate order fields
+	// compose order
+	// save order into the ledger
+	return shim.Success(nil)
+}
+
+func (cc *SupplyChainChaincode) acceptOrder(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// args: order id
+	// check role == Supplier
+	// check order existence
+	// check order status (should not be taken by another Supplier)
+	// compose contract
+	// update order status
+	// save order to common ledger
+	// save contract to Buyer-Supplier collection
+	return shim.Success(nil)
+}
+
+func (cc *SupplyChainChaincode) generateProof(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// args: auditor name/id/etc, contract id, fields included in proof (true/false for each of the contract's fields)
+	// check role == Supplier
+	// check proof existence (to avoid multiple generations)
+	// check contract existence
+	// generate proof
+	// save proof to Buyer-Supplier collection
+	return shim.Success(nil)
+}
+
+func (cc *SupplyChainChaincode) acceptContract(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// args: contract id, (optional) Auditor's or Buyer's docs
+	// check role == Auditor or Buyer
+	// if Buyer:
+	//   set contract status to "waiting for payment"
+	//   update contract (with acceptance details as well)
+	// if Auditor:
+	//   compose an acceptance details struct (docs, accepted/rejected, reason...)
+	//   store info in the Buyer-Supplier collection
+	return shim.Success(nil)
+}
+
+func (cc *SupplyChainChaincode) rejectContract(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// args: contract id, (optional) Auditor's or Buyer's docs
+	// check role == Auditor or Buyer
+	// if Buyer:
+	//   set contract status to "rejected"
+	//   update contract (with acceptance details as well)
+	// if Auditor:
+	//   compose an acceptance details struct (docs, accepted/rejected, reason...)
+	//   store info in the Buyer-Supplier collection
+	return shim.Success(nil)
+}
+
+func (cc *SupplyChainChaincode) listOrders(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// list all of the orders in common channel
+	// (optional) filter entries by status
+	return shim.Success(nil)
+}
+
+func (cc *SupplyChainChaincode) listContracts(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// check role == Buyer or Supplier
+	// list all of the contracts for the caller from all collections
+	return shim.Success(nil)
+}
+
+func (cc *SupplyChainChaincode) getProofForContract(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// args: contract id
+	// check role == Auditor
+	// check proof for contract id and Auditor's name/id/etc existence
+	// validate proof
+	// return proof
+	return shim.Success(nil)
+}
+
+func (cc *SupplyChainChaincode) listAcceptances(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// args: contract id
+	// list all acceptance details structs bound to the contract
+	return shim.Success(nil)
+}
+
+func main() {
+	err := shim.Start(new(SupplyChainChaincode))
+	if err != nil {
+		logger.Error(err.Error())
+	}
+}
---
+++ chaincode/go/trade-finance-chaincode/trade_finance_chaincode.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
+)
+
+var logger = shim.NewLogger("TradeFinanceChaincode")
+
+type TradeFinanceChaincode struct {
+}
+
+func (cc *TradeFinanceChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	logger.Debug("Init")
+
+	return shim.Success(nil)
+}
+
+func (cc *TradeFinanceChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	logger.Debug("Invoke")
+
+	function, args := stub.GetFunctionAndParameters()
+	if function == "registerInvoice" {
+		// Supplier (or Buyer?) adds an invoice to Trade-Finance CC ledger
+		return cc.registerInvoice(stub, args)
+	} else if function == "placeInvoice" {
+		// Invoice owner places an invoice on the dashboard
+		return cc.placeInvoice(stub, args)
+	} else if function == "removeInvoice" {
+		// Invoice owner removes the invoice from the dashboard
+		return cc.removeInvoice(stub, args)
+	} else if function == "placeBid" {
+		// Factor places a bid for the invoice
+		return cc.placeBid(stub, args)
+	} else if function == "editBid" {
+		// Factor edits the bid
+		return cc.editBid(stub, args)
+	} else if function == "cancelBid" {
+		// Factor cancels the bid
+		return cc.cancelBid(stub, args)
+	} else if function == "acceptBid" {
+		// Invoice owner accepts the bid; ownership of the invoice is transferred to Factor; Buyer is notified about changes
+		return cc.acceptBid(stub, args)
+	} else if function == "listBids" {
+		// List all bids (for testing purposes
+		return cc.listBids(stub, args)
+	} else if function == "listBidsForInvoice" {
+		// List all bids for the invoice
+		return cc.listBidsForInvoice(stub, args)
+	} else if function == "listInvoices" {
+		// List all invoices
+		return cc.listInvoices(stub, args)
+	}
+	// (optional) add other query functions
+
+	fnList := "{placeInvoice, removeInvoice, placeBid, editBid, cancelBid, acceptBid, " +
+		"listBids, listBidsForInvoice, listInvoices}"
+	message := fmt.Sprintf("invalid invoke function name: expected one of %s, got %s", fnList, function)
+	logger.Debug(message)
+
+	return pb.Response{Status: 400, Message: message}
+}
+
+func (cc *TradeFinanceChaincode) registerInvoice(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// args: invoice fields
+	// check role == Buyer or Supplier
+	// validate args, including owner/buyer coincidence with caller
+	// fill invoice from args
+	// save invoice
+	return shim.Success(nil)
+}
+
+func (cc *TradeFinanceChaincode) placeInvoice(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// args: invoice id
+	// check specified invoice existence
+	// check if caller is invoice owner
+	// check invoice due date
+	// check invoice trade status
+	// update invoice trade status
+	// save invoice
+	return shim.Success(nil)
+}
+
+func (cc *TradeFinanceChaincode) removeInvoice(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// args: invoice id
+	// check specified invoice existence
+	// check if caller is invoice owner
+	// check invoice trade status
+	// update invoice trade status
+	// save invoice
+	return shim.Success(nil)
+}
+
+// TODO: decide whether we need to have a possibility to query all bids after acceptance or not
+// related changes: state machine for bids
+
+func (cc *TradeFinanceChaincode) placeBid(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// check if caller is Factor
+	// check specified invoice existence
+	// check caller != owner
+	// check invoice trade status
+	// compose a bid from args
+	// save bid
+	return shim.Success(nil)
+}
+
+func (cc *TradeFinanceChaincode) editBid(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// check specified bid existence
+	// check if caller is bid creator
+	// edit bid
+	// save bid
+	return shim.Success(nil)
+}
+
+func (cc *TradeFinanceChaincode) cancelBid(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// check specified bid existence
+	// check if caller is bid creator
+	// delete bid
+	return shim.Success(nil)
+}
+
+func (cc *TradeFinanceChaincode) acceptBid(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	// check specified bid existence
+	// check specified invoice existence
+	// check if caller is invoice owner
+	// check invoice trade status
+	// check invoice due date
+	// update invoice owner and trade status
+	// save invoice
+	// delete all bids for the invoice
+	return shim.Success(nil)
+}
+
+func (cc *TradeFinanceChaincode) listBids(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
+}
+
+func (cc *TradeFinanceChaincode) listBidsForInvoice(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
+}
+
+func (cc *TradeFinanceChaincode) listInvoices(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	return shim.Success(nil)
+}
+
+func main() {
+	err := shim.Start(new(TradeFinanceChaincode))
+	if err != nil {
+		logger.Error(err.Error())
+	}
+}
