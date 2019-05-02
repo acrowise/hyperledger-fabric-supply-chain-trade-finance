f71b54a8d676094b7eb876f789e610b76d6983a6
dmtrc <elderlybill@gmail.com>
2019-05-02 14:41:00 +0300

    login styles

--- tfsc/src/Login.js
+++ tfsc/src/Login.js
@@ -1,38 +1,57 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import { Card, Elevation } from '@blueprintjs/core';
+import {
+  Button, Overlay, FormGroup, InputGroup, Card, Label
+} from '@blueprintjs/core';
 
 import { AuthConsumer } from './context/auth';
 import { actors } from './constants';
+import Nav from './containers/Nav';
+import Icon from './components/Icon/Icon';
 
 const Login = () => (
   <AuthConsumer>
     {({ isAuth, login }) => (!isAuth ? (
-        <div
-          style={{
-            display: 'flex',
-            width: '70vw',
-            flexWrap: 'wrap',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            paddingTop: '10vh'
-          }}
-        >
-          {actors.map(({ role, description }) => (
-            <Card
-              key={role.toString()}
-              interactive
-              elevation={Elevation.THREE}
-              style={{ width: '300px', height: '250px', margin: '10px' }}
-              onClick={() => {
-                login(role.toLowerCase());
-              }}
+      <>
+      <Nav />
+      <div className="login">
+        <Card className="modal" style={{ width: '670px' }}>
+          <div className="modal-header">
+            Log in
+          </div>
+          <div className="modal-body">
+            <div className="role-list">
+              {actors.map(({ role, description }) => (
+                <div
+                  key={role.toString()}
+                  className="role-item"
+                  title={description}
+                  onClick={() => {
+                    login(role.toLowerCase());
+                  }}
+                >
+              <span className="role-item-icon">
+                <Icon name={role}/>
+              </span>
+                  <div className="role-item-txt">
+                    {role}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="modal-footer">
+            <Button
+              large
+              intent="primary"
+              className="btn-modal"
             >
-              <h2>{role}</h2>
-              <p style={{ maxWidth: '300px' }}>{description}</p>
-            </Card>
-          ))}
-        </div>
+              Log in
+            </Button>
+          </div>
+        </Card>
+      </div>
+      </>
     ) : (
         <Redirect to="/dashboard" />
     ))
