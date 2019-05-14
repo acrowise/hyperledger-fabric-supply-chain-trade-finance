835baa962d83f1c4206c2547da1456392d025960
Ilia Okhotnikov <ilia.okhotnikov@altoros.com>
2019-05-14 09:59:50 +0300

    Move svgs to Icon component

--- tfsc/src/components/ActionCompleted/ActionCompleted.js
+++ tfsc/src/components/ActionCompleted/ActionCompleted.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import {Icon, Spinner} from '@blueprintjs/core';
+import { Icon, Spinner } from '@blueprintjs/core';
 import './actionCompleted.scss';
 
-const ActionCompleted = ({res, action, result}) => {
+import Icons from '../Icon/Icon';
+
+const ActionCompleted = ({ res, action, result }) => {
   const getIcon = res.complete ? (
-    <svg
-      width='42'
-      height='42'
-      viewBox='0 0 42 42'
-      fill='none'
-      className="action-completed-icon"
-    >
-      <circle cx='21' cy='21' r='20' stroke='#69D7BC' strokeWidth='2'/>
-      <path d='M14 22.4483L18.4211 27L28 16' stroke='#69D7BC' strokeWidth='4'
-            strokeLinecap='round' strokeLinejoin='round'/>
-    </svg>
+    <Icons name="complete" />
   ) : (
-    <Icon
-      icon='cross'
-      intent="danger"
-      iconSize="42"
-      className="action-completed-icon"
-    />
+    <Icon icon="cross" intent="danger" iconSize="42" className="action-completed-icon" />
   );
 
   if (!res.pending && !res.complete && !res.data) {
@@ -30,17 +17,12 @@ const ActionCompleted = ({res, action, result}) => {
   return (
     <div className="action-completed">
       {res.pending ? (
-        <Spinner
-          large
-          intent="primary"
-        />
+        <Spinner large intent="primary" />
       ) : (
         <>
-        {getIcon}
-        <p>{action}</p>
-        <div className="action-completed-result">
-          {result}
-        </div>
+          {getIcon}
+          <p>{action}</p>
+          <div className="action-completed-result">{result}</div>
         </>
       )}
     </div>
--- tfsc/src/containers/Nav.js
+++ tfsc/src/containers/Nav.js
@@ -7,6 +7,7 @@ import { useSocket } from 'use-socketio';
 import logo from '../logo.svg';
 import { title } from '../mocks';
 import Profile from '../components/Icon/Profile';
+import Icon from '../components/Icon/Icon';
 
 const Nav = ({ role, logout }) => {
   const [notifications, setNotifications] = useState([]);
@@ -22,22 +23,7 @@ const Nav = ({ role, logout }) => {
     <div className="header-notifications">
       {notifications.map((n, i) => (
         <div key={i.toString()} className="header-notifications-item">
-          <svg
-            width="18"
-            height="15"
-            viewBox="0 0 18 15"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            style={{ flex: '0 0 18px' }}
-          >
-            <path
-              d="M2 8.44828L6.42105 13L16 2"
-              stroke="#69D7BC"
-              strokeWidth="4"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+          <Icon name="tick" />
           <p style={{ paddingLeft: '15px' }}>{title[n.type]}</p>
         </div>
       ))}
