0aec676f94e728377365ab2ec7a0212f93d19139
aliaksandr.dubinin <aliaksandr.dubinin@altoros.com>
2019-05-30 19:16:13 +0300

    set peer for get requests

--- tfsc/src/helper/api.js
+++ tfsc/src/helper/api.js
@@ -2,6 +2,7 @@ import { usePost, useGet } from './hooks';
 import { METHODS_MAP } from '../constants';
 
 const mockApi = process.env.api;
+const state = window.__STATE__;
 
 console.log('process.env.api', process.env.api);
 
@@ -19,8 +20,7 @@ export const post = method => () => {
 
 export const get = (method) => {
   const params = METHODS_MAP.find(i => i.ccMethod === method);
-
   return useGet(
-    mockApi ? `/${params.ccMethod}` : `/api/channels/${params.channel}/chaincodes/${params.chaincode}?fcn=${params.ccMethod}`
+    mockApi ? `/${params.ccMethod}` : `/api/channels/${params.channel}/chaincodes/${params.chaincode}?peer=${state.org}/peer0&fcn=${params.ccMethod}`
   );
 };
